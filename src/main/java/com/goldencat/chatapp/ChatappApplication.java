7f7490e86d27baade941fb2831026ae1a62cac9b
vscharan <charanv369@gmail.com>
2025-06-05 11:41:52 +0530

    i have updated the ChatappApplication.java file to run in the tomcat server

--- src/main/java/com/goldencat/chatapp/ChatappApplication.java
+++ src/main/java/com/goldencat/chatapp/ChatappApplication.java
@@ -2,9 +2,16 @@ package com.goldencat.chatapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class ChatappApplication {
+public class ChatappApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ChatappApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatappApplication.class, args);
